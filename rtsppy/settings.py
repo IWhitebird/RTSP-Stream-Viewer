0d9e32c60cb7ba4f0405e223c5e64d135a832e90
Shreyas Patange <115157819+IWhitebird@users.noreply.github.com>
2025-05-18 20:27:03 +0530

    Update settings.py

--- rtsppy/settings.py
+++ rtsppy/settings.py
@@ -139,6 +139,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://rtsp-stream-app.onrender.com"
 ]
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 
